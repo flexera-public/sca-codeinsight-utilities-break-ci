0d4a10606bd9a89986a59c43e136444d7b1e71e3
Sean Geary <sgeary@revenera.com>
2023-01-12 06:38:58 -0500

    Add example bat calling script

---
+++ check_scan_results.bat
@@ -0,0 +1,2 @@
+rem  ${Quaified Path to Python 3.x} ${Qualified Path to check_scan_results.py} %1 %2 %3
+"C:\python\python310\python.exe" "D:\sca-codeinsight-utilities-break-ci\check_scan_results.py" %1 %2 %3
